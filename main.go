5b553c5b27386068d062241926daa3a3fea214ed
isaac2006J <164034878+isaac2006J@users.noreply.github.com>
2024-03-19 09:12:45 -0600

    Update main.go
    menu secundario

--- main.go
+++ main.go
@@ -3,5 +3,12 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Print("")
+	fmt.Println("1-Motocicleta ")
+	fmt.Println("2- Liviano ")
+	fmt.Println("3- Carga Liviana ")
+	fmt.Println("4- Carga Pesada ")
+	fmt.Println("5- Agrícola ")
+	fmt.Println("6- - Casa Rodante ")
+	fmt.Println("7- Buses ")
+	fmt.Println("8- Regresar a menú principal ")
 }
